5c05f72ff0ffe5ea354b0171f61b5f23ab62c5c6
blaineh <hamiltonbl@byui.edu>
2024-01-06 20:55:19 -0700

    Added initial script

---
+++ provision_vm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#  An automation script to provision AWS EC2 instance into sandbox account.  This will get your AWS account ID and then request access to the DB AMI.  Then it will run a Terraform script to provision an EC2 instance.
+
+#  grant access to AWS AMI
+account_id=$(aws sts get-caller-identity --query "Account" --output text)
+echo "Your AWS account ID is: $account_id"
+read -p "Enter your BYUI email address with format {SSSNNNNN@byui.edu} S=letter, N=number:" email
+echo "Your email: $email"
+curl=$(curl -s -X PUT -H "Content-Type: application/json" -d "{  \"email\": \"$email\",  \"accountId\": \"$account_id\" }" "https://ooy1dmgurf.execute-api.us-west-2.amazonaws.com/prod")
+statusCode=$(echo $curl | jq -r '.statusCode')
+if (( $statusCode != 204 )); then
+	echo -e "An error occurred granting you permissions to the VM image.  Error: $curl\nContact your TA or instructor with this error for help."
+fi
+
+#  Create a directory for Terraform and change into the directory.  Then install Terraform.account_id=$(aws sts get-caller-identity --query "Account" --output text)
+if ! [ -d ~/tf ]; then
+	unzip terraform_1.6.6_linux_amd64.zip
+	mkdir ~/bin
+	mv terraform ~/bin
+	cd tf
+else
+	echo "Terraform is already in the terminal.  The script is exiting to prevent overwriting existing scripts."
+	return
+fi
+
+
+
